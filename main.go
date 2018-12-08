42539e1a16a408e21fdb5c6fb2249466531aae95
echernukha <egor.chernukha@gmail.com>
2018-12-08 04:20:53 -0500

    added graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/echernukha/gocourse/simplevideoserver/handlers"
 
@@ -17,8 +20,37 @@ func main() {
 		defer file.Close()
 	}
 
+	killSignalCh := getKillSignalCh()
 	serverUrl := ":8000"
+	srv := startServer(serverUrl)
 	log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
+
+	waitForKillSignal(killSignalCh)
+	srv.Shutdown(context.Background())
+}
+
+func startServer(serverUrl string) *http.Server {
 	router := handlers.Router()
-	log.Fatal(http.ListenAndServe(serverUrl, router))
+	srv := &http.Server{Addr: serverUrl, Handler: router}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	return srv
+}
+
+func getKillSignalCh() chan os.Signal {
+	osKillSignalChan := make(chan os.Signal, 1)
+	signal.Notify(osKillSignalChan, os.Interrupt, syscall.SIGTERM)
+	return osKillSignalChan
+}
+
+func waitForKillSignal(killSignalChan <-chan os.Signal) {
+	killSignal := <-killSignalChan
+	switch killSignal {
+	case os.Interrupt:
+		log.Info("got SIGINT...")
+	case syscall.SIGTERM:
+		log.Info("got SIGTERM...")
+	}
 }
